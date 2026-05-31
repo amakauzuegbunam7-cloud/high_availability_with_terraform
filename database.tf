738032747c5479cd78d6e11efb17feb050b99094
amakauzuegbunam7 <amakauzuegbunam7@gmail.com>
2026-05-31 01:52:57 +0100

    update database.tf

--- database.tf
+++ database.tf
@@ -13,3 +13,6 @@ resource "aws_db_instance" "default" {
   db_subnet_group_name = aws_db_subnet_group.mydb_subnet_group.name
   skip_final_snapshot  = true
 }
+
+
+https://github.com/amakauzuegbunam7-cloud/high_availability_with_terraform.git
